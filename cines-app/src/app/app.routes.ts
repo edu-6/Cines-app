f2c51eef67c8ad24eb2bee292c30bf068c5f3698
edu-6 <edudiazdl326@gmail.com>
2025-10-15 20:37:17 -0600

    Creación de formulario login

--- cines-app/src/app/app.routes.ts
+++ cines-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InicioSesion } from '../pages/inicio-sesion/inicio-sesion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: InicioSesion
+    }
+];
