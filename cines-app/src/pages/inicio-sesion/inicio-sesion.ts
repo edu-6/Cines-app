76ea000d85568b2459cadaaea591f9b1f28a85d9
edu-6 <edudiazdl326@gmail.com>
2025-10-16 19:31:47 -0600

    Resolución de bug en formulario login

--- cines-app/src/pages/inicio-sesion/inicio-sesion.ts
+++ cines-app/src/pages/inicio-sesion/inicio-sesion.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-inicio-sesion',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inicio-sesion.html',
   styleUrl: './inicio-sesion.css'
 })
 export class InicioSesion {
+  newEventForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.newEventForm = this.formBuilder.group({
+      correo:[null, [Validators.required]],
+      contraseña:[null, [Validators.required]]
+    })
+    
+  }
+
+  submit (): void {
+    console.log("se han enviado las credenciales");
+  }
+
+  reset(): void {
+    this.newEventForm.reset();
+  }
 }
+
+
