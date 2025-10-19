9df62c0b9508e52934158861ab566a648117ea66
edu-6 <edudiazdl326@gmail.com>
2025-10-18 23:18:11 -0600

    Creación del formulario nuevo usuario

--- cines-app/src/components/usuario/nuevo-usuario-form/nuevo-usuario-form.ts
+++ cines-app/src/components/usuario/nuevo-usuario-form/nuevo-usuario-form.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nuevo-usuario-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './nuevo-usuario-form.html',
 })
 export class NuevoUsuarioForm {
+  newEventForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+
+  }
+
+  ngOnInit(): void {
+    this.newEventForm = this.formBuilder.group({
+      correo:[null, [Validators.required]],
+      contraseña:[null, [Validators.required]],
+      nombre :[null, [Validators.required]]
+    })
+    
+  }
+
+  submit (): void {
+    console.log("se han enviado las credenciales");
+  }
+
+  reset(): void {
+    this.newEventForm.reset();
+  }
 
 }
--- cines-app/src/pages/usuario/nuevo-usuario-page/nuevo-usuario-page.ts
+++ cines-app/src/pages/usuario/nuevo-usuario-page/nuevo-usuario-page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NuevoUsuarioForm } from "../../../components/usuario/nuevo-usuario-form/nuevo-usuario-form";
 
 @Component({
   selector: 'app-nuevo-usuario-page',
-  imports: [],
+  imports: [NuevoUsuarioForm],
   templateUrl: './nuevo-usuario-page.html'
 })
 export class NuevoUsuarioPage {
 
 }
+
